c4e64a2f18eeafbbee6df0357358eeec77be70c9
Filipe Maia <filopemaia2001@gmail.com>
2021-04-17 18:52:12 -0300

    falta criar Classe itens_pedido, Pedido

---
+++ BD_Projeto_Livraria/livraria/Intens_pedido/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IntensPedidoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Intens_pedido'
--- BD_Projeto_Livraria/livraria/Livro/models.py
+++ BD_Projeto_Livraria/livraria/Livro/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Livro(models.Model):
-    titulo = models.CharField(max_length=45, null=false)
-    autor = models.CharField(max_length=45, null=false)
-    editora = models.CharField(max_length=45, null=false)
-    ano
-    status
-    preco
+    titulo = models.CharField(max_length=45, null=False)
+    autor = models.CharField(max_length=45, null=False)
+    editora = models.CharField(max_length=45, null=False)
+    ano = models.PositiveIntegerField(default = 0)
+    #status models.PositiveIntegerField(default = 0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
